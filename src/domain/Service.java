278003cf4bfa392b5567d7b197f6d7c96c685c16
kvartborg <hello@kvartborg.net>
2018-04-18 21:04:42 +0200

    Add Service class

---
+++ src/domain/Service.java
@@ -0,0 +1,124 @@
+package domain;
+
+public class Service {
+  /**
+   * Name of service
+   */
+  private String name;
+
+  /**
+   * Description of a Service
+   */
+  private String description;
+
+  /**
+   * Frequency of the service
+   */
+  private int frequency;
+
+  /**
+   * The service unit, can be daily, monthly and yearly
+   * TODO could be a Enum
+   */
+  private int unit;
+
+  /**
+   * Price unit price of service
+   */
+  private int price;
+
+  /**
+   * How often should the service be repeated
+   */
+  private int repetition;
+
+  /**
+   * Get name
+   * @return name
+   */
+  public String getName () {
+    return this.name;
+  }
+
+  /**
+   * Get description
+   * @return description
+   */
+  public String getDescription () {
+    return this.description;
+  }
+
+  /**
+   * Get frequency
+   * @return [description]
+   */
+  public int getFrequency () {
+    return this.frequency;
+  }
+
+  /**
+   * Get unit
+   * @return unit
+   */
+  public int getUnit () {
+    return this.unit;
+  }
+
+  /**
+   * Get price
+   * @return price
+   */
+  public int getPrice () {
+    return this.price;
+  }
+
+  /**
+   * Get repetition
+   * @return repetition
+   */
+  public int getRepetition () {
+    return this.repetition;
+  }
+
+  /**
+   * Set name
+   */
+  public void setName (String name) {
+    this.name = name;
+  }
+
+  /**
+   * Set description
+   */
+  public void setDescription (String description) {
+    this.description = description;
+  }
+
+  /**
+   * Set frequency
+   */
+  public void setFrequency (int frequency) {
+    this.frequency = frequency;
+  }
+
+  /**
+   * Set unit
+   */
+  public void setUnit (int unit) {
+    this.unit = unit;
+  }
+
+  /**
+   * Set price
+   */
+  public void setPrice (int price) {
+    this.price = price;
+  }
+
+  /**
+   * Set repetition
+   */
+  public void setRepetition (int repetition) {
+    this.repetition = repetition;
+  }
+}
