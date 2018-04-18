76c0666447cc2334ffd88da0fe5c430f5d779fdb
kvartborg <hello@kvartborg.net>
2018-04-18 16:13:59 +0200

    Lowercase package structure

--- src/Bootstrap.java
+++ src/Bootstrap.java
@@ -3,8 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package starter;
-
 import acq.IDatabase;
 import acq.IDomain;
 import acq.IGUI;
