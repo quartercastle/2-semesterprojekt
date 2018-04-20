7660621b7e4bebbf2509f629c82ee98d6f8c6be8
JS&CSS <32328015+NJohansen@users.noreply.github.com>
2018-04-20 11:44:19 +0200

    Changes Date to GregorianCalendar

--- src/domain/Effort.java
+++ src/domain/Effort.java
@@ -1,6 +1,6 @@
 package domain;
 
-import java.util.Date;
+import java.util.GregorianCalendar;
 
 /**
  * Effort
@@ -15,12 +15,12 @@ public class Effort {
   /**
    * Start date of effort
    */
-  private Date startDate;
+  private GregorianCalendar startDate;
 
   /**
    * Start date of effort
    */
-  private Date endDate;
+  private GregorianCalendar endDate;
 
   /**
    * Company responsible of effort
@@ -35,7 +35,7 @@ public class Effort {
    * @param endDate
    * @param responsible
    */
-  public Effort(int totalPrice, Date startDate, Date endDate, Company responsible) {
+  public Effort(int totalPrice, GregorianCalendar startDate, GregorianCalendar endDate, Company responsible) {
     this.totalPrice = totalPrice;
     this.startDate = startDate;
     this.endDate = endDate;
@@ -65,7 +65,7 @@ public class Effort {
    *
    * @return startDate
    */
-  public Date getStartDate() {
+  public GregorianCalendar getStartDate() {
     return startDate;
   }
 
@@ -74,7 +74,7 @@ public class Effort {
    *
    * @param startDate
    */
-  public void setStartDate(Date startDate) {
+  public void setStartDate(GregorianCalendar startDate) {
     this.startDate = startDate;
   }
 
@@ -83,7 +83,7 @@ public class Effort {
    *
    * @return endDate
    */
-  public Date getEndDate() {
+  public GregorianCalendar getEndDate() {
     return endDate;
   }
 
@@ -92,7 +92,7 @@ public class Effort {
    *
    * @param endDate
    */
-  public void setEndDate(Date endDate) {
+  public void setEndDate(GregorianCalendar endDate) {
     this.endDate = endDate;
   }
 
