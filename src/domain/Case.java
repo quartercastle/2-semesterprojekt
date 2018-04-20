dab644cab461f25beb0d6dd78bc4e2160274d46f
Victor Gram Thomsen <31857458+grammeaway@users.noreply.github.com>
2018-04-20 11:09:54 +0200

    Log and case added (#38)
    * Log and case added
    
    Initial creation of log and case
    
    * Added setters and getters
    
    Added setters and getters for all attributes, subject to change
    
    * Addjustment to getter and setter
    
    minor adjustment
    
    * Added javadoc
    
    Javadoc comments added
    
    * Javadoc elaborated
    
    Javadoc added to the class and the attributes
    
    * Author removed from all classes
    
    Removed author from all classes, added JavaDoc to Acq-classes

---
+++ src/domain/Case.java
@@ -0,0 +1,121 @@
+/**
+ * Case class, containing the data relevant for each individual case
+ */
+package domain;
+
+import java.util.UUID;
+
+public class Case {
+
+  /**
+   * Unique ID for the case
+   */
+  private UUID id;
+  /**
+   * The user responsile for the case
+   */
+  private User responsible;
+  /**
+   * Citizen attached to the case
+   */
+  private Citizen citizen;
+  /**
+   * Effort attached to the case
+   */
+  private Effort effort;
+  /**
+   * Participating citizens
+   */
+  private Citizen participants;
+
+  /**
+   * Returns case ID
+   *
+   * @return id
+   */
+  public UUID getId() {
+    return id;
+  }
+
+  /**
+   * Sets case-id
+   *
+   * @param id
+   */
+  public void setId(UUID id) {
+    this.id = id;
+  }
+
+  /**
+   * returns the case-responsible user
+   *
+   * @return responsible
+   */
+  public User getResponsible() {
+    return responsible;
+  }
+
+  /**
+   * Sets the case-responsible user
+   *
+   * @param responsible
+   */
+  public void setResponsible(User responsible) {
+    this.responsible = responsible;
+  }
+
+  /**
+   * Returns citizen
+   *
+   * @return citizen
+   */
+  public Citizen getCitizen() {
+    return citizen;
+  }
+
+  /**
+   * sets citizen
+   *
+   * @param citizen
+   */
+  public void setCitizen(Citizen citizen) {
+    this.citizen = citizen;
+  }
+
+  /**
+   * Returns the effort created for the case
+   *
+   * @return effort
+   */
+  public Effort getEffort() {
+    return effort;
+  }
+
+  /**
+   * Sets the effort asigned to the case
+   *
+   * @param effort
+   */
+  public void setEffort(Effort effort) {
+    this.effort = effort;
+  }
+
+  /**
+   * Returns the participants in the case
+   *
+   * @return participants
+   */
+  public Citizen getParticipants() {
+    return participants;
+  }
+
+  /**
+   * Sets participants in the case
+   *
+   * @param participants
+   */
+  public void setParticipants(Citizen participants) {
+    this.participants = participants;
+  }
+
+}
