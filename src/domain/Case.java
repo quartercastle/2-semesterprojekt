b53c056340d64a15abc624171fc804b3c44c244e
Victor Gram Thomsen <31857458+grammeaway@users.noreply.github.com>
2018-04-20 10:12:37 +0200

    Log and case added
    Initial creation of log and case

---
+++ src/domain/Case.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package domain;
+
+import java.util.UUID;
+
+/**
+ *
+ * @author Victor Gram
+ */
+public class Case {
+
+  private UUID id;
+  private User responsible;
+  private Citizen citizen;
+  private Effort effort;
+  private Citizen participants;
+
+}
---
+++ src/domain/Log.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package domain;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Victor Gram
+ */
+public class Log {
+
+  private Date date;
+  private User user;
+  private Case c;
+  private String description;
+  private String oldValue;
+  private String newValue;
+
+}
