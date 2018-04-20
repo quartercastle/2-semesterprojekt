41e1ec2607beca5425852ff6261321dd67106918
Ladremarq <32633539+Ladremarq@users.noreply.github.com>
2018-04-20 11:19:44 +0200

    Effort-Offer start (#39)
    * Effort-Offer start
    
    * Added javadoc
    
    * Javadoc made right
    
    * Javadoc made righter
    
    * Ét enkelt T rettet

---
+++ src/domain/Effort.java
@@ -0,0 +1,114 @@
+package domain;
+
+/**
+ * Effort
+ */
+public class Effort {
+
+  /**
+   * Total price of effort
+   */
+  private int totalPrice;
+
+  /**
+   * Start date of effort
+   */
+  private Date startDate;
+
+  /**
+   * Start date of effort
+   */
+  private Date endDate;
+
+  /**
+   * Company responsible of effort
+   */
+  private Company responsible;
+
+  /**
+   * Constructor for efforts
+   *
+   * @param totalPrice
+   * @param startDate
+   * @param endDate
+   * @param responsible
+   */
+  public Effort(int totalPrice, Date startDate, Date endDate, Company responsible) {
+    this.totalPrice = totalPrice;
+    this.startDate = startDate;
+    this.endDate = endDate;
+    this.responsible = responsible;
+  }
+
+  /**
+   * Get total price
+   *
+   * @return totalPrice
+   */
+  public int getTotalPrice() {
+    return totalPrice;
+  }
+
+  /**
+   * Set total price
+   *
+   * @param totalPrice
+   */
+  public void setTotalPrice(int totalPrice) {
+    this.totalPrice = totalPrice;
+  }
+
+  /**
+   * Get start date
+   *
+   * @return startDate
+   */
+  public Date getStartDate() {
+    return startDate;
+  }
+
+  /**
+   * Set start date
+   *
+   * @param startDate
+   */
+  public void setStartDate(Date startDate) {
+    this.startDate = startDate;
+  }
+
+  /**
+   * Get end date
+   *
+   * @return endDate
+   */
+  public Date getEndDate() {
+    return endDate;
+  }
+
+  /**
+   * Set end date
+   *
+   * @param endDate
+   */
+  public void setEndDate(Date endDate) {
+    this.endDate = endDate;
+  }
+
+  /**
+   * Get responsible company
+   *
+   * @return responsible
+   */
+  public Company getResponsible() {
+    return responsible;
+  }
+
+  /**
+   * Set responsible company
+   *
+   * @param responsible
+   */
+  public void setResponsible(Company responsible) {
+    this.responsible = responsible;
+  }
+}
---
+++ src/domain/Offer.java
@@ -0,0 +1,86 @@
+package domain;
+
+public class Offer {
+
+  /**
+   * Name of offer
+   */
+  private String name;
+
+  /**
+   * Price of offer
+   */
+  private int price;
+
+  /**
+   * Description of offer
+   */
+  private String description;
+
+  /**
+   * Constructor for offers
+   *
+   * @param name
+   * @param price
+   * @param description
+   */
+  public Offer(String name, int price, String description) {
+    this.name = name;
+    this.price = price;
+    this.description = description;
+  }
+
+  /**
+   * Get name
+   *
+   * @return name
+   */
+  public String getName() {
+    return name;
+  }
+
+  /**
+   * Set name
+   *
+   * @param name
+   */
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  /**
+   * Get price
+   *
+   * @return price
+   */
+  public int getPrice() {
+    return price;
+  }
+
+  /**
+   * Set price
+   *
+   * @param price
+   */
+  public void setPrice(int price) {
+    this.price = price;
+  }
+
+  /**
+   * Get description
+   *
+   * @return description
+   */
+  public String getDescription() {
+    return description;
+  }
+
+  /**
+   * Set description
+   *
+   * @param description
+   */
+  public void setDescription(String description) {
+    this.description = description;
+  }
+}
