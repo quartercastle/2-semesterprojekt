4d741c7e4cf627221d7ddb66a766d87ca4c23c9a
NJohansen <32328015+NJohansen@users.noreply.github.com>
2018-04-25 13:19:29 +0200

    Add json objects

--- src/data/JavaToJson.java
+++ src/data/JavaToJson.java
@@ -2,6 +2,7 @@ package data;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
@@ -13,11 +14,21 @@ import java.io.Writer;
 public class JavaToJson {
 
   public static void main(String[] args) throws IOException {
-    try (Writer writer = new FileWriter("Output.json", true)) {
 
-      Gson gson = new GsonBuilder().create();
-      gson.toJson("Hello", writer);
-      gson.toJson(132, writer);
+    try (Writer writer = new FileWriter("Output.json", true)) {
+      Gson gson = new GsonBuilder().setPrettyPrinting().create();
+      JsonObject innerObject = new JsonObject();
+
+      innerObject.addProperty("UUID", "12312");
+      innerObject.addProperty("USER", "Niels");
+      innerObject.addProperty("Citizen", "Peter");
+      innerObject.addProperty("Effort", "Test");
+      innerObject.addProperty("Participants", "Test");
+
+      JsonObject jsonObject = new JsonObject();
+      jsonObject.add("Case 2", innerObject);
+
+      gson.toJson(jsonObject, writer);
 
       writer.close();
     }
