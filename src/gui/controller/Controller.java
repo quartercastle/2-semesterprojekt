e322b6de0ea267720f791019b6fcbb6c3a153f51
MadsDueKristensen <32197042+MadsDueKristensen@users.noreply.github.com>
2018-04-26 10:42:50 +0200

    Change

--- src/gui/controller/Controller.java
+++ src/gui/controller/Controller.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package gui.controller;
 
 import java.net.URL;
@@ -10,10 +5,6 @@ import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
 import javafx.stage.Stage;
 
-/**
- *
- * @author yodamester42
- */
 public class Controller implements Initializable {
 
   private Stage stage;
