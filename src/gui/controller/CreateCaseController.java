8eacb8b68ebdb751a191e53fbdd2b9425bdf9016
Victor Gram Thomsen <31857458+grammeaway@users.noreply.github.com>
2018-04-30 12:22:51 +0200

    Next-button implemented
    With example tab

--- src/gui/controller/CreateCaseController.java
+++ src/gui/controller/CreateCaseController.java
@@ -1,11 +1,16 @@
-
 package gui.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TabPane;
 
 public class CreateCaseController extends Controller {
+
+  @FXML
+  private TabPane caseTabPane;
 
   /**
    * Initializes the controller class.
@@ -13,6 +18,11 @@ public class CreateCaseController extends Controller {
   @Override
   public void initialize(URL url, ResourceBundle rb) {
     // TODO
+  }
+
+  @FXML
+  private void nextButtonClicked(ActionEvent event) {
+    caseTabPane.getSelectionModel().selectNext();
   }
 
 }
