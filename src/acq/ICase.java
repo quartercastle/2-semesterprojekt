d4f7148d383879b3797f5017b163cd2a6013ef02
NJohansen <32328015+NJohansen@users.noreply.github.com>
2018-05-02 12:06:31 +0200

    Added getters and setters to interface

--- src/acq/ICase.java
+++ src/acq/ICase.java
@@ -85,4 +85,200 @@ public interface ICase {
    */
   void setInformedAboutDuties(boolean informed);
 
+  /**
+   * Get informed about if they need support for practical tasks
+   *
+   * @return needPracticalTasksSupport
+   */
+  boolean needPracticalTasksSupport();
+
+  /**
+   * Set whether the citizen need support to practical tasks
+   *
+   * @param needSupport
+   */
+  void setSupportPracticalTasks(boolean needSupport);
+
+  /**
+   * Get informed about if the citizen need support for personal care
+   *
+   * @return needPersonalCareSupport
+   */
+  boolean needPersonalCareSupport();
+
+  /**
+   * Set whether the citizen needs support to personal care
+   *
+   * @param needSupport
+   */
+  void setSupportPersonalCare(boolean needSupport);
+
+  /**
+   * Get informed about if the citizen need support for grosery shopping
+   *
+   * @return needGroserySupport
+   */
+  boolean needGroserySupport();
+
+  /**
+   * Set whether the citizen needs support for grosery shopping
+   *
+   * @param needSupport
+   */
+  void setSupportGrosery(boolean needSupport);
+
+  /**
+   * Get informed if the citizen needs support for a temporary stay
+   *
+   * @return needTemporaryStay
+   */
+  boolean needTemporaryStay();
+
+  /**
+   * Set whether the citizen needs support for a temporary stay
+   *
+   * @param needTemporaryStay
+   */
+  void setTemporaryStay(boolean needTemporaryStay);
+
+  /**
+   * Get informed if the citizen needs support for a longer stay
+   *
+   * @return needLongerStay
+   */
+  boolean needLongerStay();
+
+  /**
+   * Set whether the citizen needs support for a longer stay
+   *
+   * @param needLongerStay
+   */
+  void setLongerStay(boolean needLongerStay);
+
+  /**
+   * Get informed if the citizen needs support for learning
+   *
+   * @return needLearningSupport
+   */
+  boolean needLearningSupport();
+
+  /**
+   * Set whether the citizen needs support for learning
+   *
+   * @param needLearningSupport
+   */
+  void setLearningSupport(boolean needLearningSupport);
+
+  /**
+   * Get informed if the citizen needs support for rehabilitation
+   *
+   * @return needLearningSupport
+   */
+  boolean needRehabilitationSupport();
+
+  /**
+   * Set whether the citizen needs support for rehabilitation
+   *
+   * @param needRehabilitationSupport
+   */
+  void setRehabilitationSupport(boolean needRehabilitationSupport);
+
+  /**
+   * Get informed if the citizen needs support for driving
+   *
+   * @return needDrivingSupport
+   */
+  boolean needDrivingSupport();
+
+  /**
+   * Set whether the citizen needs support for rehabilitation
+   *
+   * @param needDrivingSupport
+   */
+  void setDrivingSupport(boolean needDrivingSupport);
+
+  /**
+   * Get informed if the citizen need an offer for a temporary house
+   *
+   * @return needTemporaryHouseOffer
+   */
+  boolean needTemporaryHouseOffer();
+
+  /**
+   * Set whether the citizen need an offer for a temporary house
+   *
+   * @param needTemporaryHouseOffer
+   */
+  void setTemporaryHouseOffer(boolean needTemporaryHouseOffer);
+
+  /**
+   * Get informed if the citizen need an offer for personal care
+   *
+   * @return needTemporaryHouseOffer
+   */
+  boolean needPersonalCareOffer();
+
+  /**
+   * Set whether the citizen need an offer for personal care
+   *
+   * @param needPersonalCareOffer
+   */
+  void setPersonalCareOffer(boolean needPersonalCareOffer);
+
+  /**
+   * Get informed if the citizen need an offer for support to grocery shopping
+   *
+   * @return needSupportGroceryOffer
+   */
+  boolean needSupportGroceryOffer();
+
+  /**
+   * Set whether the citizen need an offer for support to grocery shopping
+   *
+   * @param needSupportGroceryOffer
+   */
+  void setSupportGroceryOffer(boolean needSupportGroceryOffer);
+
+  /**
+   * Get informed if the citizen need an offer for a longer house stay
+   *
+   * @return needSupportGroceryOffer
+   */
+  boolean needLongerStayOffer();
+
+  /**
+   * Set whether the citizen need an offer for a longer house stay
+   *
+   * @param needLongerStayOffer
+   */
+  void setLongerStayOffer(boolean needLongerStayOffer);
+
+  /**
+   * Get informed if the citizen need an offer for learning
+   *
+   * @return needLearningOffer
+   */
+  boolean needLearningOffer();
+
+  /**
+   * Set whether the citizen need an offer for learning
+   *
+   * @param needLearningOffer
+   */
+  void setLearningOffer(boolean needLearningOffer);
+
+  /**
+   * Get informed if the citizen need an offer for Rehabilitation
+   *
+   * @return needLearningOffer
+   */
+  boolean needRehabilitationOffer();
+
+  /**
+   * Set whether the citizen need an offer for Rehabilitation
+   *
+   * @param needRehabilitationOffer
+   */
+  void setRehabilitationOffer(boolean needRehabilitationOffer);
+
 }
