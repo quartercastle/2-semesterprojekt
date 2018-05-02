026c874442083f1049612a43567534e1ff844f58
NJohansen <32328015+NJohansen@users.noreply.github.com>
2018-05-02 12:07:13 +0200

    added getters and setters for case

--- src/domain/Case.java
+++ src/domain/Case.java
@@ -48,6 +48,76 @@ public class Case implements ICase {
   private boolean informedAboutDuties;
 
   /**
+   * Does the citizen need support for practical tasks, from Case-opening
+   */
+  private boolean practicalTasksSupport;
+
+  /**
+   * Does the citizen need support for personal care, from Case-opening
+   */
+  private boolean personalCareSupport;
+
+  /**
+   * Does the citizen need support for grosery shopping, from Case-opening
+   */
+  private boolean groserySupport;
+
+  /**
+   * Does the citizen need support for temporary stay, from Case-opening
+   */
+  private boolean temporaryStay;
+
+  /**
+   * Does the citizen need support for longer stay, from Case-opening
+   */
+  private boolean longerStay;
+
+  /**
+   * Does the citizen need support for learning support, from Case-opening
+   */
+  private boolean learningSupport;
+
+  /**
+   * Does the citizen need support for rehabilitation, from Case-opening
+   */
+  private boolean rehabilitationSupport;
+
+  /**
+   * Does the citizen need support for dring, from Case-opening
+   */
+  private boolean drivingSupport;
+
+  /**
+   * Does the citizen need an offer for a temporary house, from Case-opening
+   */
+  private boolean temporaryHouseOffer;
+
+  /**
+   * Does the citizen need an offer for personal care, from Case-opening
+   */
+  private boolean personalCareOffer;
+
+  /**
+   * Does the citizen need an offer for grocery shopping, from Case-opening
+   */
+  private boolean supportGroceryOffer;
+
+  /**
+   * Does the citizen need an offer for a longer stay, from Case-opening
+   */
+  private boolean longerStayOffer;
+
+  /**
+   * Does the citizen need an offer for learning school help, from Case-opening
+   */
+  private boolean learningOffer;
+
+  /**
+   * Does the citizen need an offer for rehabilitation, from Case-opening
+   */
+  private boolean rehabilitationOffer;
+
+  /**
    * Case constructor
    *
    * @param responsible
@@ -215,6 +285,286 @@ public class Case implements ICase {
   @Override
   public void setInformedAboutDuties(boolean informed) {
     this.informedAboutDuties = informed;
+  }
+
+  /**
+   * Get informed about if they need support for practical tasks
+   *
+   * @return needPracticalTasksSupport
+   */
+  @Override
+  public boolean needPracticalTasksSupport() {
+    return this.practicalTasksSupport;
+  }
+
+  /**
+   * Set whether the citizen need support to practical tasks
+   *
+   * @param needSupport
+   */
+  @Override
+  public void setSupportPracticalTasks(boolean needSupport) {
+    this.practicalTasksSupport = needSupport;
+  }
+
+  /**
+   * Get informed about if the citizen need support for personal care
+   *
+   * @return needPersonalCareSupport
+   */
+  @Override
+  public boolean needPersonalCareSupport() {
+    return this.personalCareSupport;
+  }
+
+  /**
+   * Set whether the citizen needs support to personal care
+   *
+   * @param needSupport
+   */
+  @Override
+  public void setSupportPersonalCare(boolean needSupport) {
+    this.personalCareSupport = needSupport;
+  }
+
+  /**
+   * Get informed about if the citizen need support for grosery shopping
+   *
+   * @return needGroserySupport
+   */
+  @Override
+  public boolean needGroserySupport() {
+    return this.groserySupport;
+  }
+
+  /**
+   * Set whether the citizen needs support for grosery shopping
+   *
+   * @param needSupport
+   */
+  @Override
+  public void setSupportGrosery(boolean needSupport) {
+    this.groserySupport = needSupport;
+  }
+
+  /**
+   * Get informed if the citizen needs support for a temporary stay
+   *
+   * @return needTemporaryStay
+   */
+  @Override
+  public boolean needTemporaryStay() {
+    return this.temporaryStay;
+  }
+
+  /**
+   * Set whether the citizen needs support for a temporary stay
+   *
+   * @param needTemporaryStay
+   */
+  @Override
+  public void setTemporaryStay(boolean needTemporaryStay) {
+    this.temporaryStay = needTemporaryStay;
+  }
+
+  /**
+   * Get informed if the citizen needs support for a longer stay
+   *
+   * @return needLongerStay
+   */
+  @Override
+  public boolean needLongerStay() {
+    return this.longerStay;
+  }
+
+  /**
+   * Set whether the citizen needs support for a longer stay
+   *
+   * @param needLongerStay
+   */
+  @Override
+  public void setLongerStay(boolean needLongerStay) {
+    this.longerStay = needLongerStay;
+  }
+
+  /**
+   * Get informed if the citizen needs support for learning
+   *
+   * @return needLearningSupport
+   */
+  @Override
+  public boolean needLearningSupport() {
+    return this.learningSupport;
+  }
+
+  /**
+   * Set whether the citizen needs support for learning
+   *
+   * @param needLearningSupport
+   */
+  @Override
+  public void setLearningSupport(boolean needLearningSupport) {
+    this.learningSupport = needLearningSupport;
+  }
+
+  /**
+   * Get informed if the citizen needs support for rehabilitation
+   *
+   * @return needLearningSupport
+   */
+  @Override
+  public boolean needRehabilitationSupport() {
+    return this.rehabilitationSupport;
+  }
+
+  /**
+   * Set whether the citizen needs support for rehabilitation
+   *
+   * @param needRehabilitationSupport
+   */
+  @Override
+  public void setRehabilitationSupport(boolean needRehabilitationSupport) {
+    this.rehabilitationSupport = needRehabilitationSupport;
+  }
+
+  /**
+   * Get informed if the citizen needs support for driving
+   *
+   * @return needDrivingSupport
+   */
+  @Override
+  public boolean needDrivingSupport() {
+    return this.drivingSupport;
+  }
+
+  /**
+   * Set whether the citizen needs support for rehabilitation
+   *
+   * @param needDrivingSupport
+   */
+  @Override
+  public void setDrivingSupport(boolean needDrivingSupport) {
+    this.drivingSupport = needDrivingSupport;
+  }
+
+  /**
+   * Get informed if the citizen need an offer for a temporary house
+   *
+   * @return needTemporaryHouseOffer
+   */
+  @Override
+  public boolean needTemporaryHouseOffer() {
+    return this.temporaryHouseOffer;
+  }
+
+  /**
+   * Set whether the citizen need an offer for a temporary house
+   *
+   * @param needTemporaryHouseOffer
+   */
+  @Override
+  public void setTemporaryHouseOffer(boolean needTemporaryHouseOffer) {
+    this.temporaryHouseOffer = needTemporaryHouseOffer;
+  }
+
+  /**
+   * Get informed if the citizen need an offer for personal care
+   *
+   * @return needTemporaryHouseOffer
+   */
+  @Override
+  public boolean needPersonalCareOffer() {
+    return this.personalCareOffer;
+  }
+
+  /**
+   * Set whether the citizen need an offer for personal care
+   *
+   * @param needPersonalCareOffer
+   */
+  @Override
+  public void setPersonalCareOffer(boolean needPersonalCareOffer) {
+    this.personalCareOffer = needPersonalCareOffer;
+  }
+
+  /**
+   * Get informed if the citizen need an offer for support to grocery shopping
+   *
+   * @return needSupportGroceryOffer
+   */
+  @Override
+  public boolean needSupportGroceryOffer() {
+    return this.supportGroceryOffer;
+  }
+
+  /**
+   * Set whether the citizen need an offer for support to grocery shopping
+   *
+   * @param needSupportGroceryOffer
+   */
+  @Override
+  public void setSupportGroceryOffer(boolean needSupportGroceryOffer) {
+    this.supportGroceryOffer = supportGroceryOffer;
+  }
+
+  /**
+   * Get informed if the citizen need an offer for a longer house stay
+   *
+   * @return needSupportGroceryOffer
+   */
+  @Override
+  public boolean needLongerStayOffer() {
+    return this.longerStayOffer;
+  }
+
+  /**
+   * Set whether the citizen need an offer for a longer house stay
+   *
+   * @param needLongerStayOffer
+   */
+  @Override
+  public void setLongerStayOffer(boolean needLongerStayOffer) {
+    this.longerStayOffer = needLongerStayOffer;
+  }
+
+  /**
+   * Get informed if the citizen need an offer for learning
+   *
+   * @return needLearningOffer
+   */
+  @Override
+  public boolean needLearningOffer() {
+    return this.learningOffer;
+  }
+
+  /**
+   * Set whether the citizen need an offer for learning
+   *
+   * @param needLearningOffer
+   */
+  @Override
+  public void setLearningOffer(boolean needLearningOffer) {
+    this.learningOffer = needLearningOffer;
+  }
+
+  /**
+   * Get informed if the citizen need an offer for Rehabilitation
+   *
+   * @return needLearningOffer
+   */
+  @Override
+  public boolean needRehabilitationOffer() {
+    return this.rehabilitationOffer;
+  }
+
+  /**
+   * Set whether the citizen need an offer for Rehabilitation
+   *
+   * @param needRehabilitationOffer
+   */
+  @Override
+  public void setRehabilitationOffer(boolean needRehabilitationOffer) {
+    this.rehabilitationOffer = needRehabilitationOffer;
   }
 
 }
