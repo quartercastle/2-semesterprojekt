f347ffe13c581d1e20d538f126e6b5088f9de3ea
NJohansen <32328015+NJohansen@users.noreply.github.com>
2018-05-18 12:15:45 +0200

    Testing removed

--- src/Bootstrap.java
+++ src/Bootstrap.java
@@ -2,7 +2,6 @@
 import acq.IData;
 import acq.IDomain;
 import data.DataFacade;
-import data.model.DataService;
 import domain.DomainFacade;
 import gui.GUI;
 
@@ -16,9 +15,6 @@ public class Bootstrap {
   public static void main(String[] args) {
     IData data = DataFacade.getInstance();
     data.initialize();
-    DataService service = DataService.find(1);
-    service.setDescription("fisse");
-    service.save();
     IDomain domain = DomainFacade.getInstance();
     domain.inject(data);
     GUI.initialize(args, domain);
