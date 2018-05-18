d6da6a3b9ed136ebf026c076e32436606b8d59ff
GGkarma <jepper01@hotmail.com>
2018-05-18 11:01:50 +0200

    Find and save method for CaseWorker

--- src/data/model/DataCaseWorker.java
+++ src/data/model/DataCaseWorker.java
@@ -3,9 +3,11 @@ package data.model;
 import acq.IAddress;
 import acq.ICaseWorker;
 import acq.IUser;
+import data.Database;
 
 public class DataCaseWorker extends DataPerson implements ICaseWorker {
 
+  private int id;
   /**
    * User in system that DataCaseWorker is connected to
    */
@@ -33,6 +35,51 @@ public class DataCaseWorker extends DataPerson implements ICaseWorker {
   @Override
   public IUser getUser() {
     return this.user;
+  }
+
+  public static DataCaseWorker find(int id) {
+    DataCaseWorker caseWorker = new DataCaseWorker(null, null, null, null, null, null);
+    Database.getInstance().query(Database.compose(
+            "SELECT id, person_id, user_id",
+            "FROM case_worker",
+            "WHERE id = " + id
+    ),
+            rs -> {
+              caseWorker.setId(rs.getInt(1));
+              DataPerson dataPerson = new DataPerson.find(rs.getint(2));
+              caseWorker.setFirstName(dataPerson.getFirstName());
+              caseWorker.setMiddleName(dataPerson.getMiddleName());
+              caseWorker.setLastName(dataPerson.getMiddleName());
+              caseWorker.setAddress(dataPerson.getAddress());
+              caseWorker.setPhone(dataPerson.getPhone());
+              caseWorker.setEmail(dataPerson.getEmail());
+              caseWorker.setUser(DataUser.find(rs.getInt(3)));
+            });
+    return caseWorker;
+  }
+
+  public void save() {
+    String query = null;
+
+    if (getId() == 0) {
+      String[] values = {
+        super.getId() + ""
+        getUser().getId() + ""
+    }
+    query = Database.compose(
+            "INSERT INTO case_workers (person_id, user_id)",
+            "VALUES"
+    )
+  }
+
+}
+
+public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
   }
 
 }
