85d070a5433d956f657ae116951b77ce23d1431f
GGkarma <32382976+GGkarma@users.noreply.github.com>
2018-05-22 14:26:49 +0200

    Update PostgreSQLConfig.java

--- src/config/PostgreSQLConfig.java
+++ src/config/PostgreSQLConfig.java
@@ -2,7 +2,7 @@ package config;
 
 public class PostgreSQLConfig {
 
-  public String host = "jdbc:postgresql://elmer.db.elephantsql.com:5432";
-  public String user = "sjekiexb";
-  public String password = "nE_DfmfRgBwapPJ7JPp_AO88AR7aV26n";
+  public String host;
+  public String user;
+  public String password;
 }
