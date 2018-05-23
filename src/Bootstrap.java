2e9e80d036cc3be8db495bfa89285a8ce2244e65
MadsDueKristensen <32197042+MadsDueKristensen@users.noreply.github.com>
2018-05-23 13:18:42 +0200

    Update Bootstrap.java

--- src/Bootstrap.java
+++ src/Bootstrap.java
@@ -1,23 +1,9 @@
 
 import acq.IData;
 import acq.IDomain;
-import acq.ServiceUnit;
 import data.DataFacade;
-import data.model.DataAddress;
-import data.model.DataCaseWorker;
-import data.model.DataCompany;
-import data.model.DataEffort;
-import data.model.DataOffer;
-import data.model.DataParagraph;
-import data.model.DataRole;
-import data.model.DataService;
-import data.model.DataUser;
 import domain.DomainFacade;
-import domain.system.CaseWorker;
-import domain.system.Effort;
 import gui.GUI;
-import java.util.GregorianCalendar;
-import util.Mapper;
 
 public class Bootstrap {
 
