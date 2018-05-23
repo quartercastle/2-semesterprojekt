9bf0c643f3b2b41521819a2b1de7e2b5821be342
NJohansen <32328015+NJohansen@users.noreply.github.com>
2018-05-23 15:22:07 +0200

    gitignore

--- src/config/PostgreSQLConfig.java
+++
@@ -1,7 +0,0 @@
-package config;
-
-public class PostgreSQLConfig {
-  public String host = "jdbc:postgresql://localhost:5432";
-  public String user = "dbuser";
-  public String password = "dbpswd";
-}
